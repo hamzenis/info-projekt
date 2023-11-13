d2a55901e96213bf803662b00fe0f1c60ea5c587
Ebrahim <bronum89@gmail.com>
2023-11-13 15:47:49 +0100

    Ebrahims Main, loginscreen and sign up route

--- info_projekt/lib/main.dart
+++ info_projekt/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:info_projekt/auth/loginScreen.dart';
-import 'firebase_options.dart';
-import 'package:info_projekt/auth/registerScreen.dart'; // Import RegisterScreen
+import 'package:info_projekt/features/splashScreen.dart';
+import 'package:info_projekt/pages/home_page.dart';
+import 'package:info_projekt/pages/login_page.dart';
+import 'package:info_projekt/pages/sign_up_page.dart'; // Import RegisterScreen
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,16 +11,24 @@ void main() async {
   runApp(const MainApp());
 }
 
+
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: RegisterScreen(), // Start with LoginScreen
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
       routes: {
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
       },
     );
   }
