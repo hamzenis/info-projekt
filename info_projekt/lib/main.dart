6854d989ccb25678644994e7cb1ce3ad91ed0d07
DomiRosario <dominique.rosario@icloud.com>
2023-11-15 10:42:57 +0100

    Update app name in main.dart

--- info_projekt/lib/main.dart
+++ info_projekt/lib/main.dart
@@ -9,11 +9,11 @@ import 'package:info_projekt/views/sign_up_page.dart'; // Import RegisterScreen
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
