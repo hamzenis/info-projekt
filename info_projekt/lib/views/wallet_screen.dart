a175472e05468c8f00fccbdd38360f5d90e0daf9
DomiRosario <dominique.rosario@icloud.com>
2023-11-21 19:39:42 +0100

    Database Integration in Wallet_Screen

--- info_projekt/lib/views/wallet_screen.dart
+++ info_projekt/lib/views/wallet_screen.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class WalletScreen extends StatefulWidget {
   @override
@@ -9,8 +11,8 @@ class WalletScreen extends StatefulWidget {
 }
 
 class _WalletScreenState extends State<WalletScreen> {
-  double _balance = 0.0;
-  List<Transaction> _transactions = [];
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
 
   Future<double?> getUserInput() async {
     final controller = TextEditingController();
@@ -70,15 +72,28 @@ class _WalletScreenState extends State<WalletScreen> {
 
       await Stripe.instance.presentPaymentSheet();
 
-      setState(() {
-        _balance += amount;
-        _transactions.add(Transaction(
-          description: 'Deposit',
-          amount: amount,
-          date: DateTime.now(),
-          type: TransactionType.deposit,
-        ));
-      });
+      final user = _auth.currentUser;
+      if (user != null) {
+        final querySnapshot = await _firestore
+            .collection('Users')
+            .where('UID', isEqualTo: user.uid)
+            .get();
+        final userDoc = querySnapshot.docs.first;
+
+        await _firestore.collection('Users').doc(userDoc.id).update({
+          'balance': FieldValue.increment(amount),
+        });
+        await _firestore
+            .collection('Users')
+            .doc(userDoc.id)
+            .collection('balance_history')
+            .add({
+          'amount': amount,
+          'date': Timestamp.now(),
+          'description': 'Deposit',
+          'withdraw': false,
+        });
+      }
     } on Exception catch (e) {
       print('Payment failed: $e');
     }
@@ -86,82 +101,154 @@ class _WalletScreenState extends State<WalletScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Wallet'),
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 16.0),
-          Text(
-            'Balance: \$${_balance.toStringAsFixed(2)}',
-            style: TextStyle(fontSize: 24.0),
+    print('User UID: ${_auth.currentUser?.uid}');
+    return FutureBuilder<QuerySnapshot>(
+      future: _firestore
+          .collection('Users')
+          .where('UID', isEqualTo: _auth.currentUser?.uid)
+          .get(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Text('No data');
+        }
+        final data = snapshot.data!.docs.first.data() as Map<String, dynamic>;
+        final balance = (data['balance'] as num).toDouble();
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('My Wallet'),
           ),
-          SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          body: Column(
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement withdraw money functionality
-                },
-                child: Text('Withdraw'),
+              SizedBox(height: 16.0),
+              Text(
+                'Balance: \$${balance.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 24.0),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  double? depositAmount = await getUserInput();
-                  if (depositAmount != null) {
-                    await startDepositFlow(depositAmount);
-                  }
-                },
-                child: Text('Deposit'),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // TODO: Implement withdraw money functionality
+                    },
+                    child: Text('Withdraw'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      double? depositAmount = await getUserInput();
+                      if (depositAmount != null) {
+                        await Future.delayed(Duration(milliseconds: 500));
+                        await startDepositFlow(depositAmount);
+                      }
+                    },
+                    child: Text('Deposit'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: FutureBuilder<QuerySnapshot>(
+                  future: _firestore
+                      .collection('Users')
+                      .where('UID', isEqualTo: _auth.currentUser?.uid)
+                      .get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> userSnapshot) {
+                    if (userSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (!userSnapshot.hasData ||
+                        userSnapshot.data!.docs.isEmpty) {
+                      return Text('No user data');
+                    }
+                    final userDoc = userSnapshot.data!.docs.first;
+                    return StreamBuilder<QuerySnapshot>(
+                      stream: _firestore
+                          .collection('Users')
+                          .doc(userDoc.id)
+                          .collection('balance_history')
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                        if (!snapshot.hasData) {
+                          return Text('No transactions');
+                        }
+                        final transactions = snapshot.data!.docs
+                            .map((doc) => Transaction.fromFirestore(doc))
+                            .toList();
+                        return ListView.builder(
+                          itemCount: transactions.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            var reversedTransactions =
+                                transactions.reversed.toList();
+                            return ListTile(
+                              title:
+                                  Text(reversedTransactions[index].description),
+                              subtitle: Text(
+                                  reversedTransactions[index].date.toString()),
+                              trailing: Text(
+                                '\$${reversedTransactions[index].amount.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  color: reversedTransactions[index].type ==
+                                          TransactionType.withdrawal
+                                      ? Colors.red
+                                      : Colors.green,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
             ],
           ),
-          SizedBox(height: 16.0),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _transactions.length,
-              itemBuilder: (BuildContext context, int index) {
-                var reversedTransactions = _transactions.reversed.toList();
-
-                return ListTile(
-                  title: Text(reversedTransactions[index].description),
-                  subtitle: Text(reversedTransactions[index].date.toString()),
-                  trailing: Text(
-                    '\$${reversedTransactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      color: reversedTransactions[index].type ==
-                              TransactionType.withdrawal
-                          ? Colors.red
-                          : Colors.green,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
 
+enum TransactionType { deposit, withdrawal }
+
 class Transaction {
-  final String description;
   final double amount;
   final DateTime date;
+  final String description;
   final TransactionType type;
 
   Transaction({
-    required this.description,
     required this.amount,
     required this.date,
+    required this.description,
     required this.type,
   });
-}
 
-enum TransactionType {
-  withdrawal,
-  deposit,
+  factory Transaction.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    final amount = (data['amount'] as num).toDouble();
+    final date = (data['date'] as Timestamp).toDate();
+    final description = data['description'] as String;
+    final type = data['type'] == 'withdrawal'
+        ? TransactionType.withdrawal
+        : TransactionType.deposit;
+
+    return Transaction(
+      amount: amount,
+      date: date,
+      description: description,
+      type: type,
+    );
+  }
 }
