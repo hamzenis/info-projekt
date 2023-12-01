fa5dd0fcde04fa16fe636129792416602db36fd4
Ebrahim Al Numayri <ebrahim.alnumayri@stud.fra-uas.de>
2023-12-01 14:35:07 +0100

    Upgraded the logic and deleted not needed logic

--- info_projekt/lib/services/firebase_auth_services.dart
+++ info_projekt/lib/services/firebase_auth_services.dart
@@ -2,42 +2,36 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:info_projekt/widgets/toast.dart';
 
 class FirebaseAuthService {
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-Future<User?> signUpWithEmailAndPassword(String email, String password) async {
-  try {
-    UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-    User? user = credential.user;
-    if (user != null && !user.emailVerified) {
-      await user.sendEmailVerification();
+  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = credential.user;
+      if (user != null && !user.emailVerified) {
+        await user.sendEmailVerification();
+      }
+      return user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        showToast(message: 'This email is already in use. Please log in or use a different email.');
+      } else {
+        showToast(message: 'An error occurred: ${e.code}');
+      }
+      return null; // Immediately return to avoid executing further lines
     }
-    return user;
-  } on FirebaseAuthException catch (e) {
-    showToast(message: 'An error occurred: ${e.code}');
-    return null; // Immediately return to avoid executing further lines
+  }
+
+  Future<UserCredential?> signInWithEmailAndPassword(String email, String password) async {
+    return await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+Future<void> resendVerificationEmail() async {
+  User? user = _auth.currentUser;
+  if (user != null && !user.emailVerified) {
+    await user.sendEmailVerification();
   }
 }
 
-Future<User?> signInWithEmailAndPassword(String email, String password) async {
-  try {
-    UserCredential credential = await _auth.signInWithEmailAndPassword(email: email, password: password);
-    User? user = credential.user;
-
-    // Check if the user's email is verified
-    if (user != null && !user.emailVerified) {
-      showToast(message: 'Please verify your email address.');
-      return null; // Stop further execution
-    }
-
-    return user; // Email is verified, return the user
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'user-not-found' || e.code == 'wrong-password' || e.code == 'invalid-credential') {
-      showToast(message: 'Invalid email or password.');
-    } else {
-      showToast(message: 'An error occurred: ${e.code}');
-    }
-    return null; // Stop further execution after handling the exception
-  }
-}
 
 }
