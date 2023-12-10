b584d9169018f8c428e3fa2c044908e081e2c6b8
Hamzenis Kryeziu <hamzenis@gmail.com>
2023-12-10 15:22:42 +0100

    Fixed Bug firebase fetching bug in owned stock page

--- info_projekt/lib/views/owned_stocks_page.dart
+++ info_projekt/lib/views/owned_stocks_page.dart
@@ -8,64 +8,80 @@ class OwnedStocksPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _firestore
+    return FutureBuilder<QuerySnapshot>(
+      future: _firestore
           .collection('Users')
-          .doc(_auth.currentUser?.uid)
-          .collection('transaction_history')
-          .where('owned', isEqualTo: true)
-          .snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          if (snapshot.hasError) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text('Owned Stocks'),
-              ),
-              body: Center(child: Text("Error: ${snapshot.error}")),
-            );
-          }
-
-          if (snapshot.hasData) {
-            final transactions = snapshot.data?.docs ?? [];
-            return Scaffold(
-              appBar: AppBar(
-                title: Text('Owned Stocks'),
-              ),
-              body: ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (context, index) {
-                  final transaction = transactions[index];
-                  return ListTile(
-                    title: Text(transaction['stock_symbol']),
-                    subtitle: Text('Amount: ${transaction['amount']}'),
-                  );
-                },
-              ),
-            );
-          } else {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text('Owned Stocks'),
-              ),
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-        } else if (snapshot.connectionState == ConnectionState.none) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Owned Stocks'),
-            ),
-            body: Center(child: Text("No data")),
-          );
-        } else {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Owned Stocks'),
-            ),
-            body: Center(child: CircularProgressIndicator()),
-          );
+          .where('UID', isEqualTo: _auth.currentUser?.uid)
+          .get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<QuerySnapshot> userSnapshot) {
+        if (userSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
         }
+        if (!userSnapshot.hasData || userSnapshot.data!.docs.isEmpty) {
+          return const Text('No user data');
+        }
+        final userDoc = userSnapshot.data!.docs.first;
+        return StreamBuilder<QuerySnapshot>(
+          stream: _firestore
+              .collection('Users')
+              .doc(userDoc.id)
+              .collection('transaction_history')
+              .where('owned', isEqualTo: true)
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasError) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text('Owned Stocks'),
+                  ),
+                  body: Center(child: Text("Error: ${snapshot.error}")),
+                );
+              }
+              if (snapshot.hasData) {
+                final transactions = snapshot.data?.docs ?? [];
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text('Owned Stocks'),
+                  ),
+                  body: ListView.builder(
+                    itemCount: transactions.length,
+                    itemBuilder: (context, index) {
+                      final transaction = transactions[index];
+                      return ListTile(
+                        title: Text(transaction['stock_symbol']),
+                        subtitle: Text('Amount: ${transaction['amount']}'),
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text('Owned Stocks'),
+                  ),
+                  body: Center(child: CircularProgressIndicator()),
+                );
+              }
+            } else if (snapshot.connectionState == ConnectionState.none) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('Owned Stocks'),
+                ),
+                body: Center(child: Text("No data")),
+              );
+            } else {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('Owned Stocks'),
+                ),
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }
+          },
+        );
       },
     );
   }
