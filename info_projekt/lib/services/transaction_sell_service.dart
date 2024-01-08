9852f3c86510da6bbde7da0bc9ae1708ede64231
Moker2 <bronum89@gmail.com>
2024-01-08 16:34:33 +0100

    added taxex at 25% for the profit and fixed the logic

--- info_projekt/lib/services/transaction_sell_service.dart
+++ info_projekt/lib/services/transaction_sell_service.dart
@@ -127,22 +127,41 @@ Future<bool> startSellStockFlow(
 // Retrieve tax_pot from Firestore
     double taxPot = (userDoc['tax_pot'] as num).toDouble();
 
-// Calculate new tax_pot
-    double profit = 0;
-    for (final stock in stockSnapshot.docs) {
-      double purchasePrice = double.tryParse(stock['price'].toString()) ?? 0.0;
-      int individualStockQuantity =
-          int.tryParse(stock['quantity'].toString()) ?? 0;
-      profit += (singlePrice - purchasePrice) * individualStockQuantity;
-    }
-    taxPot += profit;
-    taxPot -= fee; // subtract fee from tax pot
+// // Calculate new tax_pot and balance
+double profit = 0;
+double loss = fee; // Start with the transaction fee as initial loss
+for (final stock in stockSnapshot.docs) {
+  double purchasePrice = double.tryParse(stock['price'].toString()) ?? 0.0;
+  int individualStockQuantity =
+      int.tryParse(stock['quantity'].toString()) ?? 0;
+  double difference = singlePrice - purchasePrice;
+  if (difference < 0) {
+    // It's a loss
+    loss += difference * individualStockQuantity;
+  } else {
+    // It's a profit
+    profit += difference * individualStockQuantity;
+  }
+}
+
+// Add loss to taxPot
+taxPot += loss;
+
+// If taxPot is positive, transfer the amount to profit
+if (taxPot > 0) {
+  profit += taxPot;
+  taxPot = 0;
+}
+
+// Tax the profit at 25% and add to balance
+double taxedProfit = profit * 0.75;
+double newBalance = userDoc['balance'] + taxedProfit;
 
 // Update balance and tax_pot in Firestore
-    await _firestore.collection('Users').doc(userDoc.id).update({
-      'balance': FieldValue.increment(totalPriceAfterFee),
-      'tax_pot': taxPot,
-    });
+await _firestore.collection('Users').doc(userDoc.id).update({
+  'balance': newBalance,
+  'tax_pot': taxPot,
+});
 
     await _firestore
         .collection('Users')
