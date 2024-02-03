4044acc4527967231bc8ce32ce4d04c8c4205a59
DomiRosario <dominique.rosario@icloud.com>
2024-02-03 21:34:07 +0100

    deleted expandablefab added button from package -> less error -> delegating is king

--- info_projekt/lib/homepage_new.dart
+++ info_projekt/lib/homepage_new.dart
@@ -6,11 +6,11 @@ import 'package:info_projekt/pages/profile_page.dart';
 import 'package:info_projekt/pages/watchlist_page.dart';
 import 'package:info_projekt/provider/portfolio.dart';
 import 'package:info_projekt/views/wallet_screen.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:provider/provider.dart';
 import 'views/search_view.dart';
 import 'views/newspage.dart';
 import 'services/portfolio_service.dart';
-import 'widgets/menubutton.dart';
 import 'widgets/tab_bar.dart';
 
 /// The home page of the app aka. the portfolio page of the user.<br />
@@ -78,19 +78,44 @@ class HomePageNewState extends State<HomePageNew> {
               ),
             ],
           ),
-          floatingActionButton: MenuButton(
-            distance: 112.0,
-            pageBuilders: [
-              (context) => const SearchPage(),
-              (context) => const NewsPage(),
-              (context) => ProfilePage(),
-              (context) => const WalletScreen(),
-            ],
-            children: const [
-              Icon(Icons.search),
-              Icon(Icons.newspaper),
-              Icon(Icons.person),
-              Icon(Icons.wallet),
+          floatingActionButton: SpeedDial(
+            animatedIcon: AnimatedIcons.menu_close,
+            animatedIconTheme: const IconThemeData(size: 22.0),
+            overlayColor: Colors.black,
+            overlayOpacity: 0.5,
+            children: [
+              SpeedDialChild(
+                child: const Icon(Icons.search),
+                label: 'Search',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SearchPage()),
+                ),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.newspaper),
+                label: 'News',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NewsPage()),
+                ),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.person),
+                label: 'Profile',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                ),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.wallet),
+                label: 'Wallet',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const WalletScreen()),
+                ),
+              ),
             ],
           ),
         ),
